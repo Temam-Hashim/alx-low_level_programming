0c0ebeffaac0b7312ff4c579542e8124751135bb
TEMAM <temamhashim3@gmail.com>
2022-04-10 23:52:57 +0530

    Final commit inside hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+	int a;
+	float b;
+	char c;
+	long l;
+	unsigned int a1;
+	unsigned long l1;
+	printf("Size of Integer=%u\n",sizeof(a));
+	printf("Size of Float=%u\n",sizeof(b));
+	printf("Size of Character=%u\n",sizeof(c));
+	printf("Size of Long=%u\n",sizeof(l));
+	printf("Size of Unsigned Integer=%u\n",sizeof(a1));
+	printf("Size of Unsigned Long=%u\n",sizeof(l1));
+
+	return (0);
+}
